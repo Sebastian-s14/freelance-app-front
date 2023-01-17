21cfb7b59d200be677d7549d2925a70ad4b2d586
Sebastian <sebastian.sanchez@tecsup.edu.pe>
2023-01-16 22:50:49 -0500

    feat: :bento: add svg icons

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as donwloadIcon } from './donwload.svg'
+export { default as eyeOff } from './eye-off.svg'
+export { default as eye } from './eye.svg'
+export { default as receiptLogo } from './receipt.svg'
